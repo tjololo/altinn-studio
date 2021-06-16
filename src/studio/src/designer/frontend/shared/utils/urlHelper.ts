30dbd99bc1e668e20133b3bcb77126d630af72dd
mjulstein <6736558+mjulstein@users.noreply.github.com>
2021-06-16 10:37:30 +0200

    refactored designer/frontend/shared urlHelper

--- src/studio/src/designer/frontend/shared/utils/urlHelper.ts
+++ src/studio/src/designer/frontend/shared/utils/urlHelper.ts
@@ -1,28 +1,25 @@
-/* tslint:disable: max-line-length */
 const altinnWindow = window as any;
 const { org, repo } = altinnWindow;
 const origin = window.location.origin;
+const cdn = 'https://altinncdn.no';
 
-export const getApplicationMetadataUrl = (): string => {
-  return `${origin}/designer/api/v1/${org}/${repo}`;
-};
-
-export const altinnAppsIllustrationHelpCircleSvgUrl = 'https://altinncdn.no/img/illustration-help-circle.svg';
-export const altinnAppsImgLogoBlueSvgUrl = 'http://altinncdn.no/img/a-logo-blue.svg';
-export const altinnDocsUrl = 'http://docs.altinn.studio/';
+export const applicationMetadataUrl = `${origin}/designer/api/v1/${org}/${repo}`;
+export const altinnAppsIllustrationHelpCircleSvgUrl = `${cdn}/img/illustration-help-circle.svg`;
+export const altinnAppsImgLogoBlueSvgUrl = `${cdn}/img/a-logo-blue.svg`;
+export const altinnDocsUrl = 'https://docs.altinn.studio/';
 export const altinnStudioDocsUrl = 'https://altinn.github.io/docs/altinn-studio/';
-export const altinnImgLogoHeaderUrl = 'https://altinncdn.no/img/altinn_logo_header.png';
+export const altinnImgLogoHeaderUrl = `${cdn}/img/altinn_logo_header.png`;
 export const dataModelUploadPageUrl = `${origin}/designer/${org}/${repo}#/datamodel`;
 export const dataModelXsdUrl = `${origin}/designer/${org}/${repo}/Model/GetXsd`;
-export const orgsListUrl: string = 'https://altinncdn.no/orgs/altinn-orgs.json';
+export const orgsListUrl = `${cdn}/orgs/altinn-orgs.json`;
 export const repositoryGitUrl = `${origin}/repos/${org}/${repo}.git`;
 export const repositoryUrl = `${origin}/repos/${org}/${repo}`;
-export const baseHostnameAltinnProd = 'altinn.no';
-export const baseHostnameAltinnTest = 'altinn.cloud';
-export const baseHostnameAltinnStudio = 'altinn3.no';
-export const pathToMessageBox = 'ui/messagebox';
 
 export const returnUrlToMessagebox = (url: string): string => {
+  const baseHostnameAltinnProd = 'altinn.no';
+  const baseHostnameAltinnTest = 'altinn.cloud';
+  const baseHostnameAltinnStudio = 'altinn3.no';
+  const pathToMessageBox = 'ui/messagebox';
   const prodRegex = new RegExp(baseHostnameAltinnProd);
   const testRegex = new RegExp(baseHostnameAltinnTest);
   const studioRegex = new RegExp(baseHostnameAltinnStudio);
